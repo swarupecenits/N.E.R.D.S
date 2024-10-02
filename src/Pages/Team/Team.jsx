92547f3b1b7c53dcea9ecf0f14dbe58ccc559807
Ezed9 <nishitbaishya9@gmail.com>
2024-10-02 15:01:27 +0530

    complete team card

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,20 +1,8 @@
-import React from "react";
-import MemberCard from '../../components/team_card'; // Adjust the import path as necessary
-import teamData from './teamData.json'; // Adjust the path to your JSON file
 
 const Team = () => {
   return (
-    <div className="bg-black text-white min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-center mb-12">Meet Our Team</h1>
-      
-      <div className="flex flex-wrap justify-center gap-8">
-        {/* Display only the first three members */}
-        {teamData.slice(0, 3).map((member, index) => (
-          <MemberCard key={index} member={member} />
-        ))}
-      </div>
-    </div>
-  );
-};
+    <div>Team</div>
+  )
+}
 
-export default Team;
+export default Team
