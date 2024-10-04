b117c7cc9289aca0f0cc468faff2e05bd3684ada
swarupecenits <swarupchanda1963@gmail.com>
2024-10-04 21:42:46 +0530

    Updates margin

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -72,9 +72,9 @@ const Team = () => {
       }}>FOURTH YEAR MEMBER</h1>
       
     </div>
-    <div className="grid grid-cols-1 mt-8 p-4  md:grid-cols-2 lg:grid-cols-3 gap-0">
+    <div className="grid grid-cols-1 mt-8 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {fourthYearMembers.map((member, index) => (
-          <TeamCard key={index} member={member} />
+          <TeamCard key={index} member={member}/>
         ))}
       </div>
     <div className="grid place-items-center"
