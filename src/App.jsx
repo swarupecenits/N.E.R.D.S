5ec2a5e9bc758873fa1cbff011e6fb7287a1e213
Anushkadam4 <damanushka@gmail.com>
2024-10-05 13:11:44 +0530

    Upcoming_events

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
+import Event from './Pages/Event/Event';
 import Team from './Pages/Team/Team';
 import Gallery from './Pages/Gallery/Gallery';
 import Contact from './Pages/Contact/Contact';
+import Navbar from './components/Navbar/navbar';
 
 function App() {
   
@@ -14,19 +15,13 @@ function App() {
       <Router>
       <div>
         <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/gallery">Gallery</Link></li>
-            <li><Link to="/team">Team</Link></li>
-          </ul>
+          <Navbar/>
         </nav>
 
         {/* Define Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/event" element={<Event />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/team" element={<Team />} />
           <Route path="/gallery" element={<Gallery />} />
