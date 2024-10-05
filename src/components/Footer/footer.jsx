344e9d2f597f81dfa7ea79bea0b2d14980444d23
Rockjone-pixel <ayushmanbaruah0101@gmail.com>
2024-10-05 23:56:55 +0530

    Made footer responsive

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,134 +1,188 @@
-import { FaFacebook } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { PiInstagramLogoFill } from "react-icons/pi";
-
+import React from 'react';
+import { FaFacebook } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { PiInstagramLogoFill } from 'react-icons/pi';
 
 function Footer() {
   return (
     <>
-    <div className='mt-20 relative flex items-center justify-center'>
-      <div
-        style={{
+      {/* Mobile Footer */}
+      <footer className="md:hidden flex flex-col items-center mt-20">
+        <div
+          className="w-screen h-[371px] absolute top-0 left-0 bg-cover bg-no-repeat"
+          style={{
+            backgroundImage: 'url("https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/tuchgn9xdbqwiesi3ywk")',
+          }}
+        >
+          <div className="w-full h-full flex flex-col items-center justify-start backdrop-blur-[40px] bg-white/10 shadow-md">
+            <div id="logo" className="w-full flex justify-center mt-8 md:mt-4">
+              <a href="/">
+                <img
+                  src="https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/npansp6k2ntw7qeoqoms"
+                  alt="Logo"
+                  className="w-28 h-28 cursor-pointer"
+                />
+              </a>
+            </div>
 
-          backgroundImage: 'url("https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/tuchgn9xdbqwiesi3ywk")', 
-          width: '100vw',
-          height: '371.09px',
-          position: 'absolute',
-          top: '0',
-          left: '0px',
-          gap: '0px',
-          opacity: '1',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-     
-        }}
-      > 
-<div 
-  className='w-full h-[68%] opacity-1 flex flex-row relative justify-between' 
-  style={{
-    backdropFilter: 'blur(40px)', 
-    backgroundColor: 'rgba(255, 255, 255, 0.1)', 
-    boxShadow: '0 4px px rgba(0, 0, 0, 0.1)',
-    WebkitBackdropFilter: 'blur(18px)'
+            <div className="flex flex-col items-center mt-4">
+              <p className="font-ethenocentric text-white text-[16.2px] font-light">Wanna know more?</p>
+              <input
+                type="email"
+                placeholder="Email us here"
+                className="mt-4 bg-[#3E3E3E] text-white text-sm w-[80%] max-w-xs p-2 border-2 border-gray-600 outline-none rounded-lg"
+              />
+              <p className="text-base text-white font-spaced mt-3">
+                Or contact us <a className="text-blue-500 underline" href="/contact">here</a>
+              </p>
+            </div>
 
-  }}
->
-  <div id='logo'>
-  <a href="/" >
-    <img 
-      src="https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/npansp6k2ntw7qeoqoms" 
-      className="w-28 h-28 ml-12 mt-14 z-50 cursor-pointer absolute top-0 left-0" 
-      
-    />
- </a>
-  </div>
+            <div className="flex flex-col items-center mt-4">
+              <p className="text-white font-ethenocentric text-[16.2px] font-bold">Visit Us</p>
+              <p className="text-white font-spaced text-[14px] font-bold">
+                <a
+                  href="https://www.google.com/maps?q=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  NIT Silchar, Silchar, Assam, India- 788010
+                </a>
+              </p>
+              <p className="text-white font-spaced text-[14px] font-bold">
+                <a
+                  href="mailto:nerds.nitsilchar@gmail.com?subject=Hello%20N.E.R.D.S TEAM"
+                >
+                  Email- nerds.nitsilchar@gmail.com
+                </a>
+              </p>
+              <p className="text-white font-spaced text-[14px] font-bold">+91-60031 XXXXX</p>
+            </div>
 
-  <div id='wanna' className='ml-32 mt-14 flex flex-col'>
-    <p 
-      className=' font-ethenocentric' 
-      style={{ fontSize: '16.2px', width: 'auto', height: '29px', fontWeight: '300',color:'#FFFFFF' }}
-    >
-      Wanna know more?
-    </p>
-
-    <input 
-      type="email" 
-      placeholder="Email us here" 
-      style={{ 
-
-        backgroundColor: '#3E3E3E', 
-        marginTop: '20px', 
-        fontSize: '14.12px', 
-        width: '20rem', 
-        padding: '8px',
-        border:'2px',
-        borderColor:'grey', 
-        color: 'white',        
-        outline: 'none',  
-        fontFamily:'spaced',     
-        clipPath: 'polygon(15px 0%, 100% 0%, 100% 100%, 0% 100%, 0% 15px)' 
-
-      }}
-    />
-
-    <p className='text-base text-white font-spaced mt-3'>
-      Or contact us <a className='text-blue-500 underline' href='/contact'>here</a>
-    </p>
-  </div>
-
-  <div style={{ width: 'auto', height: '140px' }} className='mt-14'>
-    <p 
-      className='text-white font-ethenocentric' 
-      style={{ fontSize: '16.2px', width: '120px', height: '29px', fontWeight: 'bold' }}
-    >
-      Visit Us
-    </p>
-    <p 
-      className='text-white mt-5 font-spaced' 
-      style={{ fontSize: '14px', width: 'auto', height: '29px', fontWeight: 'bold'}}
-    >
-      <a href="https://www.google.com/maps?q=1600+Amphitheatre+Parkway,+Mountain+View,+CA" target="_blank">NIT Silchar, Silchar, Assam, India- 788010</a>
-
-      
-    </p>
-    <p 
-      className='text-white mt-0 font-spaced' 
-      style={{ fontSize: '14px', width: 'auto', height: '29px', fontWeight: 'bold'}}
-    >
-      <a href="mailto:nerds.nitsilchar@gmail.com?subject=Hello%20N.E.R.D.S TEAM">Email- nerds.nitsilchar@gmail.com</a>
-
-    </p>
-    <p 
-      className='text-white mt-0 font-spaced' 
-      style={{ fontSize: '14px', width: 'auto', height: '29px', fontWeight: 'bold'}}
-    >
-      +91-60031 XXXXX
-    </p>
-  </div>
-
-  <div style={{ width: '16px', height: '140px', marginRight: '110px' }} className='mt-14 flex flex-col'>
-
-    <div><a target="_blank" href='https://www.facebook.com/roboticsclub.nits?mibextid=ZbWKwL'><FaFacebook color='white' size="30px" /></a></div>
-    <div className='mt-6'><a target="_blank" href='https://www.instagram.com/nerds.nitsilchar?igsh=MTM0MjFpZjF6M3Zucg=='><PiInstagramLogoFill color='white' size="30px" /></a></div>
-    <div className='mt-6'><a target="_blank" href='https://www.linkedin.com/company/n-e-r-d-s-nits/'><FaLinkedin color='white' size="30px" /></a></div>
-
-  </div>
-
-</div>
-        <div className=' w-full h-[25%] flex flex-row justify-center'>
-
-        <div className="mt-20 font-spaced text-center">
-  <h1 style={{ fontWeight: 'bold', fontSize: '1rem' }} className="text-white">
-    © 2024 N.E.R.D.S. NIT Silchar. All Rights Reserved.
-  </h1>
-</div>
-
-
+            <div className="flex justify-center mt-4">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.facebook.com/roboticsclub.nits?mibextid=ZbWKwL"
+                className="mr-4"
+              >
+                <FaFacebook color="white" size="30px" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.instagram.com/nerds.nitsilchar?igsh=MTM0MjFpZjF6M3Zucg=="
+                className="mr-4"
+              >
+                <PiInstagramLogoFill color="white" size="30px" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/company/n-e-r-d-s-nits/"
+              >
+                <FaLinkedin color="white" size="30px" />
+              </a>
+            </div>
+          </div>
         </div>
 
-  </div>
-    
-</div>
+        <div className="mt-20 font-spaced text-center">
+          <h1 className="text-white font-bold text-[1rem]">
+            © 2024 N.E.R.D.S. NIT Silchar. All Rights Reserved.
+          </h1>
+        </div>
+      </footer>
+
+      {/* Laptop Footer */}
+      <div className='hidden md:flex mt-20 relative flex items-center justify-center'>
+        <div id="background"
+          className='w-screen h-[371px] absolute top-0 left-0 bg-cover bg-no-repeat opacity-100'
+          style={{
+            backgroundImage: 'url("https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/tuchgn9xdbqwiesi3ywk")',
+          }}
+        >
+          <div className='w-full h-[68%] opacity-100 flex flex-row relative justify-between backdrop-blur-[40px] bg-white/10 shadow-md'>
+            <div id='logo' className="ml-12 mt-14">
+              <a href="/">
+                <img
+                  src="https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/npansp6k2ntw7qeoqoms"
+                  className="w-28 h-28 z-50 cursor-pointer absolute top-0 left-0"
+                />
+              </a>
+            </div>
+
+            <div id='wanna' className='ml-32 mt-14 flex flex-col'>
+              <p className='font-ethenocentric text-white text-[16.2px] h-[29px] font-light'>
+                Wanna know more?
+              </p>
+
+              <input
+                type="email"
+                placeholder="Email us here"
+                style={{
+                  backgroundColor: '#3E3E3E',
+                  marginTop: '20px',
+                  fontSize: '14.12px',
+                  width: '20rem',
+                  padding: '8px',
+                  border: '2px',
+                  borderColor: 'grey',
+                  color: 'white',
+                  outline: 'none',
+                  fontFamily: 'spaced',
+                  clipPath: 'polygon(15px 0%, 100% 0%, 100% 100%, 0% 100%, 0% 15px)',
+                }}
+              />
+
+              <p className='text-base text-white font-spaced mt-3'>
+                Or contact us <a className='text-blue-500 underline' href='/contact'>here</a>
+              </p>
+            </div>
+
+            <div id="visit_us" className='mt-14 w-auto h-[140px]'>
+              <p className='text-white font-ethenocentric text-[16.2px] w-[120px] h-[29px] font-bold'>
+                Visit Us
+              </p>
+              <p className='text-white mt-5 font-spaced text-[14px] w-auto h-[29px] font-bold'>
+                <a href="https://www.google.com/maps?q=1600+Amphitheatre+Parkway,+Mountain+View,+CA" target="_blank">
+                  NIT Silchar, Silchar, Assam, India- 788010
+                </a>
+              </p>
+              <p className='text-white mt-0 font-spaced text-[14px] w-auto h-[29px] font-bold'>
+                <a href="mailto:nerds.nitsilchar@gmail.com?subject=Hello%20N.E.R.D.S TEAM">Email- nerds.nitsilchar@gmail.com</a>
+              </p>
+              <p className='text-white mt-0 font-spaced text-[14px] w-auto h-[29px] font-bold'>
+                +91-60031 XXXXX
+              </p>
+            </div>
+
+            <div id="icons" className='mt-14 flex flex-col w-[16px] h-[140px] mr-[110px]'>
+              <div>
+                <a target="_blank" href='https://www.facebook.com/roboticsclub.nits?mibextid=ZbWKwL'>
+                  <FaFacebook color='white' size="30px" />
+                </a>
+              </div>
+              <div className='mt-6'>
+                <a target="_blank" href='https://www.instagram.com/nerds.nitsilchar?igsh=MTM0MjFpZjF6M3Zucg==' >
+                  <PiInstagramLogoFill color='white' size="30px" />
+                </a>
+              </div>
+              <div className='mt-6'>
+                <a target="_blank" href='https://www.linkedin.com/company/n-e-r-d-s-nits/'>
+                  <FaLinkedin color='white' size="30px" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='w-full h-[25%] flex flex-row justify-center'>
+            <div id="copyright" className="mt-20 font-spaced text-center">
+              <h1 className="text-white font-bold text-[1rem]">
+                © 2024 N.E.R.D.S. NIT Silchar. All Rights Reserved.
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
