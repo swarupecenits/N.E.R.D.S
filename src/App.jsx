ce6b7724f23b2985aaf916f61d06ebb01439292b
Deepanshu Sharma <sharmadeepanshu9901@gmail.com>
2024-10-07 00:25:27 +0530

    navbar-sticky

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,34 @@ import Team from './Pages/Team/Team';
 import Gallery from './Pages/Gallery/Gallery';
 import Contact from './Pages/Contact/Contact';
 import Navbar from './components/Navbar/navbar';
-
+import Footer from './components/Footer/footer';
 
 function App() {
+  
   return (
     <div>
       <Router>
-        
+      <div>
         <div className="sticky top-0 z-50">
-          <nav>
-            <Navbar />
-          </nav>
+        <nav >
+          <Navbar/>
+        </nav>
         </div>
 
-        
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/event" element={<Event />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/gallery" element={<Gallery />} />
-          </Routes>
-        </div>
-      </Router>
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+
+        <Footer/>
+      </div>
+    </Router>
     </div>
-  );
+  )
 }
 
 export default App;
