47b6f2ebe248628de355e3413d1dd6e39afa3f80
Hraday Sadrani <113025213+hradaysadrani@users.noreply.github.com>
2024-10-06 15:01:39 +0530

    basic layout of contactus page was constructed

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,58 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './ContactForm.scss'
 
+export const Contact = () => {
+  const form = useRef();
 
-const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_a8haiq8', 'template_4lap528', form.current, {
+        publicKey: '9XVcWsYdwTJqGLNOA',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact-page">
+    <div className="contact-form-container">
+      <h2>Contact Us</h2>
+      <form  onSubmit={sendEmail}>
+        <input
+          type="text"
+          name="from_name"
+          placeholder="Enter Your Name"
+          required
+        />
+        <input
+          type="email"
+          name="from_email"
+          placeholder="Enter Your Email"
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Type Your Message"
+          required
+        ></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
+
+
+// email - nerdsnits@gmail.com
+// password - NERDS@swarup
