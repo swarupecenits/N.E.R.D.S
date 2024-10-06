bb410da0d5b418a62d19c4fbc9cf75b5e319d0bb
Nishit Baishya <nishit@Nishits-MacBook-Air.local>
2024-10-06 15:51:22 +0530

    basic components of testimonial done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
+import TestimonialCard from "../../components/testimonial/testimonial_card";
+import TestimonialText from "../../components/testimonial/testimonial_text";
+
 const Home = () => {
+  const testimonialCard = {
+    name: "Dr. Chandrajit Choudhary",
+    role: "Faculty Incharge",
+    image: "https://res.cloudinary.com/diabjuzqc/image/upload/f_auto,q_auto/vyqmiibr51fbjie0l4dk",
+  };
+
+  const testimonialText = {
+    text: "This program has significantly enhanced my skills and has provided me with a platform to showcase my knowledge and talents."
+  };
+
   return (
-    <div>
-      <h1 className="font-ethenocentric text-3xl font-bold underline text-white">
-      Hello NERDS!
-    </h1>
-    
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      {/* Combine the TestimonialCard and TestimonialText in the same flex container */}
+      <div className="flex flex-row">
+        {/* Testimonial Card */}
+        <TestimonialCard testimonial={testimonialCard} />
+
+        {/* Testimonial Text */}
+        <TestimonialText testimonial={testimonialText} />
+      </div>
     </div>
-  )
+  );
+};
 
-
-}
-
-export default Home
+export default Home;
