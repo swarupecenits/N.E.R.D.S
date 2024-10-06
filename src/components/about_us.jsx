8863bf00099d652607172ebfc0e90ba37e8b2165
abhinav8723 <abhinav8723@gmail.com>
2024-10-07 03:23:59 +0530

    font orieneetation glassmorphism

--- src/components/about_us.jsx
+++ src/components/about_us.jsx
@@ -4,7 +4,7 @@ const AboutUs = () => {
   return (
     <div className="relative min-h-screen bg-black flex flex-col justify-start">
       <div
-        className="absolute inset-0 bg-no-repeat bg-cover bg-blend-normal"
+        className="absolute inset-0 bg-no-repeat bg-cover bg-blend-normal bg-black opacity-50"
         style={{
           backgroundImage: `url('https://res.cloudinary.com/dqmktpekh/image/upload/f_auto,q_auto/uzsmwldv4xpfv2xkxceg')`,
           backgroundSize: 'cover',
@@ -38,23 +38,31 @@ const AboutUs = () => {
       <div className="flex flex-col items-center px-4 md:px-20 mt-8">
         {/* First Box - Introduction */}
         <div
-          className="relative z-10 mt-8 mx-auto p-6 shadow-lg w-full md:w-2/3 hover:shadow-[0_0_25px_10px_#0083ff] bg-black bg-opacity-80 hover:bg-opacity-60 transition-all duration-100"
+          className="relative z-10 mt-8 mx-auto p-6 shadow-lg w-full md:w-2/3 backdrop-blur-md hover:shadow-[0_0_25px_10px_#0083ff] hover:backdrop-blur-none hover:bg-black hover:bg-opacity-50 transition-all duration-500"
           style={{
-            border: '4px solid #8a8787',
+            border: '5px solid #8a8787',
             borderRadius: '5%',
-            padding: '16px',
-            marginLeft: '2%', // Shift left slightly
+            marginLeft: '4%', // Shift box slightly left
           }}
         >
-          <h4 className="text-lg font-semibold text-gray-100 font-spaced"
-          style={{
-            fontSize: '210%',
-          }}
-          >Introduction</h4>
-          <p className="mt-2 text-gray-100  py-5 font-calibri"
-          style={{
-            fontSize: '150%',
-          }}
+          <h4
+            className="text-lg font-semibold text-gray-100 font-spaced sm:text-2xl md:text-4xl lg:text-6xl"
+            style={{
+              paddingLeft: '7%',
+              paddingTop: '4%',
+            }}
+          >
+            Introduction
+          </h4>
+          <p
+            className="mt-2 text-gray-100 py-5 font-spaced leading-tight text-sm md:text-base lg:text-3xl"
+            style={{
+              paddingLeft: '7%', // Shift the text inside the first box to the right
+              marginRight: '7%',
+              paddingTop: '4%',
+              paddingBottom: '5%',
+              lineHeight: '1.2', // Adjust line height for closer lines
+            }}
           >
             On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So
             Beguiled And Demoralized By The Charms Of Pleasure Of The Moment, So Blinded By Desire,
@@ -66,25 +74,33 @@ const AboutUs = () => {
 
         {/* Second Box - Our Mission */}
         <div
-          className="relative z-10 mt-8 mx-auto p-6 shadow-lg w-full md:w-2/3 hover:shadow-[0_0_25px_10px_#ff00e8] bg-black bg-opacity-80 hover:bg-opacity-60 transition-all duration-100"
+          className="relative z-10 mt-8 mx-auto p-6 shadow-lg w-full md:w-2/3 hover:shadow-[0_0_25px_10px_#ff00e8] backdrop-blur-md hover:shadow-[0_0_25px_10px_#0083ff] hover:backdrop-blur-none hover:bg-black hover:bg-opacity-50 transition-all duration-500"
           style={{
             textAlign: 'right',
             border: '4px solid #8a8787',
             borderRadius: '5%',
-            padding: '16px',
             marginRight: '2%', // Shift right slightly
             marginTop: '4%',
           }}
         >
-          <h2 className="text-lg font-semibold text-gray-100 font-spaced"
-          style={{
-            fontSize: '210%',
-          }}
-          >What we do?</h2>
-          <p className="mt-2 text-gray-100  py-5"
-          style={{
-            fontSize: '150%',
-          }}
+          <h2
+            className="text-lg font-semibold text-gray-100 font-spaced text-2xl md:text-3xl lg:text-6xl"
+            style={{
+              paddingRight: '7%',
+              paddingTop: '4%',
+            }}
+          >
+            What we do?
+          </h2>
+          <p
+            className="mt-2 text-gray-100 py-5 font-spaced leading-tight text-sm md:text-base lg:text-3xl"
+            style={{
+              paddingRight: '7%',
+              paddingTop: '4%',
+              marginLeft: '7%',
+              paddingBottom: '5%',
+              lineHeight: '1.2', // Adjust line height for closer lines
+            }}
           >
             On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So
             Beguiled And Demoralized By The Charms Of Pleasure Of The Moment, So Blinded By Desire,
@@ -96,25 +112,33 @@ const AboutUs = () => {
 
         {/* Third Box - Contact Us */}
         <div
-          className="relative z-10 mt-8 mx-auto p-6 shadow-lg w-full md:w-2/3 hover:shadow-[0_0_25px_10px_#13ff00] bg-black bg-opacity-80 hover:bg-opacity-60 transition-all duration-100"
+          className="relative z-10 mt-8 mx-auto p-6 shadow-lg w-full md:w-2/3 hover:shadow-[0_0_25px_10px_#13ff00] backdrop-blur-md hover:shadow-[0_0_25px_10px_#0083ff] hover:backdrop-blur-none hover:bg-black hover:bg-opacity-50 transition-all duration-500"
           style={{
             border: '4px solid #8a8787',
             borderRadius: '5%',
-            padding: '16px',
             marginLeft: '2%', // Shift left slightly
             marginBottom: '2%',
             marginTop: '4%',
           }}
         >
-          <h2 className="text-lg font-semibold text-gray-100  font-spaced"
-          style={{
-            fontSize: '210%',
-          }}
-          >What can you expect?</h2>
-          <p className="mt-2 text-gray-100 text-2xl py-5"
-          style={{
-            fontSize: '150%',
-          }}
+          <h2
+            className="text-lg font-semibold text-gray-100 font-spaced text-2xl md:text-3xl lg:text-6xl"
+            style={{
+              paddingLeft: '7%', // Shift the text inside the first box to the right
+              paddingTop: '4%',
+            }}
+          >
+            What can you expect?
+          </h2>
+          <p
+            className="mt-2 text-gray-100 py-5 font-spaced leading-tight text-sm md:text-base lg:text-3xl"
+            style={{
+              paddingLeft: '7%', // Shift the text inside the first box to the right
+              marginRight: '7%',
+              paddingTop: '4%',
+              paddingBottom: '5%',
+              lineHeight: '1.2', // Adjust line height for closer lines
+            }}
           >
             On The Other Hand, We Denounce With Righteous Indignation And Dislike Men Who Are So
             Beguiled And Demoralized By The Charms Of Pleasure Of The Moment, So Blinded By Desire,
