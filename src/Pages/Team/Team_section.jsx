2eb7da56415a1f2c1c202f1176c3f1dea5ea0f39
JACKIE30 <jubaraj.t3006@gmail.com>
2024-10-08 22:38:48 +0530

    changes

--- src/Pages/Team/Team_section.jsx
+++ src/Pages/Team/Team_section.jsx
@@ -9,14 +9,19 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import data from "./teamData.json";
+import datas from "./developerdata.json"
 
 const Team = () => {
   const [isLaptopView, setIsLaptopView] = useState(window.innerWidth >= 1024);
 
   const [teamData, setTeamData] = useState([]);
+  const [developerData, setDeveloperData]=useState([])
 
   useEffect(() => {
     setTeamData(data);
+  }, []);
+  useEffect(() => {
+    setDeveloperData(datas);
   }, []);
 
   const checkScreenSize = () => {
@@ -32,9 +37,12 @@ const Team = () => {
     };
   }, []);
 
+
+
   const fourthYearMembers = teamData.filter((member) => member.team === "4th Year");
   const thirdYearMembers = teamData.filter((member) => member.team === "3rd Year");
   const secondYearMembers = teamData.filter((member) => member.team === "2nd Year");
+  const developers = developerData.filter((member) => member.developer_team === "Yes");
 
   return (
     <>
@@ -65,6 +73,20 @@ const Team = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="grid place-items-center"
+        style={{
+          marginTop: "84px",
+          marginBottom: "30px",
+        }}
+      >
+        <hr className="firstline" />
+        <hr className="secondline" />
+        <hr className="thirdline" />
+      </div>
+
+
+
 
       <div className="text-center" style={{ marginTop: "250px" }}>
         <h1 className="font-ethenocentric font-normal bg-gradient-to-b from-[#ffffff] to-[#068bf7] bg-clip-text text-transparent meetour">
@@ -103,6 +125,18 @@ const Team = () => {
         </Swiper>
       )}
 
+<div
+        className="grid place-items-center"
+        style={{
+          marginTop: "84px",
+          marginBottom: "30px",
+        }}
+      >
+        <hr className="firstline" />
+        <hr className="secondline" />
+        <hr className="thirdline" />
+      </div>
+
       <div className="text-center" style={{ marginTop: "250px" }}>
         <h1 className="font-ethenocentric font-normal bg-gradient-to-b from-[#ffffff] to-[#068bf7] bg-clip-text text-transparent meetour">
           MEET OUR
@@ -138,8 +172,58 @@ const Team = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-        
+
       )}
+      <div
+        className="grid place-items-center"
+        style={{
+          marginTop: "84px",
+          marginBottom: "30px",
+        }}
+      >
+        <hr className="firstline" />
+        <hr className="secondline" />
+        <hr className="thirdline" />
+      </div>
+
+      <div className="text-center" style={{ marginTop: "250px" }}>
+        <h1 className="font-ethenocentric font-normal bg-gradient-to-b from-[#ffffff] to-[#068bf7] bg-clip-text text-transparent meetour">
+          MEET OUR
+        </h1>
+        <h1 className="font-ethenocentric font-normal bg-gradient-to-b from-[#ffffff] to-[#068bf7] bg-clip-text text-transparent member">
+        DEVELOPERS
+        </h1>
+      </div>
+
+      {isLaptopView ? (
+        <div className="grid grid-cols-1 mt-8 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-6">
+          {developers.map((member, index) => (
+            <TeamCard key={index} member={member} />
+          ))}
+        </div>
+      ) : (
+        <Swiper
+          navigation
+          pagination={{ clickable: true }}
+          grabCursor={true}
+          spaceBetween={20}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 1, spaceBetween: 10 },
+            768: { slidesPerView: 2, spaceBetween: 20 },
+            1024: { slidesPerView: 3, spaceBetween: 30 },
+          }}
+          modules={[Navigation, Pagination, EffectCoverflow]}
+        >
+          {developers.map((member, index) => (
+            <SwiperSlide key={index}>
+              <TeamCard member={member} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+      )}
+
     </>
   );
 };
