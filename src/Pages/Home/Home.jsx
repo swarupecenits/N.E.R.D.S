2f7713653fe6420cdbea5269fcdd2dd13bcfa64f
abhinav8723 <abhinav8723@gmail.com>
2024-10-08 12:53:07 +0530

    updatein dev

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutUs from '../../components/about_try';
 import React from 'react';
 const Home = () => {
   return (
@@ -5,6 +6,7 @@ const Home = () => {
       <h1 className="font-ethenocentric text-3xl font-bold underline">
       Hello NERDS!
     </h1>
+    <AboutUs></AboutUs>
     </div>
   )
 
