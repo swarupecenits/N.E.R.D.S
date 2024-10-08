280a36fe1ddda186e5414badfb446633415283d9
Swarup Chanda <swarupchanda1963@gmail.com>
2024-10-08 12:56:34 +0530

    Updates push

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TestimonialSection from "../../components/testimonial/testimonial_section"; // Adjust the path if needed
-import WhatDoWeProvide from "../../components/what_do_we_provide/what_do_we_provide_card"; // Adjust the import name
+// import WhatDoWeProvide from "../../components/what_do_we_provide/what_do_we_provide_card"; // Adjust the import name
 import WhatDoWeProvideSlider from "../../components/what_do_we_provide/what_do_we_provide_section";
+import Projects from "../../components/Projects/Projects";
+
 
 const Home = () => {
   return (
     <div>
-      {/* Other sections of the home page */}
+      <Projects/>
 
       {/* What Do We Provide Section */}
       <WhatDoWeProvideSlider />
