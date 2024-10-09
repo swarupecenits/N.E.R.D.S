e204e45ee2caeeaebcf439eed130ee985711a0ae
Nishit Baishya <nishit@Nishits-MacBook-Air.local>
2024-10-09 17:15:08 +0530

    completed upcoming events and contact us

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,17 @@ import TestimonialSection from "../../components/testimonial/testimonial_section
 // import WhatDoWeProvide from "../../components/what_do_we_provide/what_do_we_provide_card"; // Adjust the import name
 import WhatDoWeProvideSlider from "../../components/what_do_we_provide/what_do_we_provide_section";
 import Projects from "../../components/Projects/Projects";
+import EventCardSlider from "../../components/upcoming_events/event_card_slider";
 
 
 const Home = () => {
   return (
     <div>
+      
+    <EventCardSlider/>
+
       <Projects/>
+
 
       {/* What Do We Provide Section */}
       <WhatDoWeProvideSlider />
