dfb754aa160e0148d1c6a8c6b22d6ebd687b0775
sunidhiyk <sunidhichetri@gmail.com>
2024-10-09 23:02:24 +0530

    final about us

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,10 +3,12 @@ import TestimonialSection from "../../components/testimonial/testimonial_section
 // import WhatDoWeProvide from "../../components/what_do_we_provide/what_do_we_provide_card"; // Adjust the import name
 import WhatDoWeProvideSlider from "../../components/what_do_we_provide/what_do_we_provide_section";
 import Projects from "../../components/Projects/Projects";
+import AboutUsSection from "../../components/About_Us/AboutUsSection";
 
 const Home = () => {
   return (
     <div>
+      <AboutUsSection></AboutUsSection>
       <Projects />
 
       {/* What Do We Provide Section */}
