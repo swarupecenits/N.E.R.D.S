e1ae4dca93080c44d93726dd5322d57eb58214f8
JACKIE30 <jubaraj.t3006@gmail.com>
2024-10-13 20:57:30 +0530

    lander

---
+++ src/Pages/Home/Landing.jsx
@@ -0,0 +1,28 @@
+import './landing.css';
+import React from 'react';
+
+
+
+const Landing = () => {
+ return(
+    <>
+
+    <div className="main-div">
+    <h1 className="typewriter">ROBOTICS CLUB, NIT SILCHAR</h1>
+
+        <img src="https://res.cloudinary.com/dmhbmurzw/image/upload/v1728710649/Subject_lvavbf.png" alt="" className='parallax-image'/>
+        <div className='nerds parallax-text font-ethenocentric bg-gradient-to-b from-[#ffffff] to-[#c7c7c7] bg-clip-text text-transparent'>
+            <h1>N</h1>
+            <h1>E</h1>
+            <h1>R</h1>
+            <h1>D</h1>
+            <h1>S</h1>
+
+        </div>
+
+    </div>
+    
+    </>
+ )
+};
+export default Landing;
