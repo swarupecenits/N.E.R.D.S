d5dfde2977834c551ca40bf46995bad62ccebb45
Swarup Chanda <swarupchanda1963@gmail.com>
2024-10-14 01:21:54 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
