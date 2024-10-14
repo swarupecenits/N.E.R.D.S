25ed6ffc8fdd6c51b6851c09c0074de397ebd48d
Uddhav <uddhavsaikia@gmail.com>
2024-10-14 19:40:29 +0530

    most issues with modal n the links have been connected

--- src/Pages/Event/Component/Modal_mini_body.jsx
+++ src/Pages/Event/Component/Modal_mini_body.jsx
@@ -3,6 +3,7 @@ import pic from "../Assets/modal/modalpic1.svg";
 import modaldata from  "../JSON/modaldata.json";
 import pc_card_border from "../Assets/pc_card_border.svg";
 import modalbg from "../Assets/modalbgmobile.svg";
+import Button from "../Component/Button"
 
 const Modal = ({ id, toggleModal }) => {
   const obj = { ...modaldata };
@@ -18,7 +19,7 @@ const Modal = ({ id, toggleModal }) => {
           alt=""
           className="absolute top-0 left-0 w-full h-full object-cover"
         /> */}
-        <div className="relative z-10 flex flex-col justify-center items-center p-6 md:p-6 pt-20">
+        <div className="relative z-10 flex flex-col justify-center items-center p-6 md:p-6 pt-10">
           <h1 className="text-center mb-4 font-ethenocentric text-3xl md:text-4xl text-blue-600">
             {obj[id].head}
           </h1>
@@ -42,12 +43,36 @@ const Modal = ({ id, toggleModal }) => {
           </p>
 
           <div className="flex justify-center mt-4">
+            <a href={obj[id].register} target="_blank" rel="noopener noreferrer">
             <button
-              onClick={toggleModal}
-              className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-            >
-              Toggle Menu
+                    className= { `mt-[rem] mr-2 -ml-5 relative inline-flex h-10 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none `}
+                  >
+                    <span
+                      className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"
+                    >
+                    </span>
+                    <span
+                      className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-4 text-md font-medium text-white backdrop-blur-3xl gap-2 undefined"
+                    >
+                      Register
+                    </span>
             </button>
+            </a>
+
+            <button
+                    className= { `mt-[rem] relative inline-flex h-10 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none `}
+                  >
+                    <span
+                      className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"
+                    >
+                    </span>
+                    <span
+                      className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-md font-medium text-white backdrop-blur-3xl gap-2 undefined"
+                    >
+                        Back
+                    </span>
+            </button>
+
           </div>
         </div>
       </div>
