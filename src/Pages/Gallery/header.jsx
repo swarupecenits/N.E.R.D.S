d4a4fcbd65c8fa83bc9dd102c874a917fece55bf
rd4554 <reechadeka@gmail.com>
2024-10-14 19:29:10 +0530

    button responsive animation opacity

--- src/Pages/Gallery/header.jsx
+++ src/Pages/Gallery/header.jsx
@@ -5,96 +5,103 @@ import { motion } from "framer-motion";
 const Header = ({ setActiveSection }) => {
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, translateX: "-100%" }}
-        whileInView={{ opacity: 1, translateX: "0%" }}
-        transition={{ duration: 1 }}
-        className="sticky top-0 left-0 w-full bg-black z-50 bg-opacity-30 flex items-center justify-start gap-4 md:gap-12 lg:gap-20 py-3 md:py-6 pl-2  md:px-12 lg:px-20 font-ethnocentric font-normal text-blue-500 z-50 mt-5 "
+     <motion.div
+  initial={{ opacity: 0, translateX: "-100%" }}
+  whileInView={{ opacity: 1, translateX: "0%" }}
+  transition={{ duration: 1 }}
+  className="sticky top-0 left-0 w-full bg-black z-50 bg-opacity-30 flex flex-col md:flex-row items-start justify-start gap-4 md:gap-12 lg:gap-20 py-3 md:py-6 pl-2 md:px-12 lg:px-20 font-ethnocentric font-normal text-blue-500 z-50 mt-5"
+>
+  <div className="grid grid-cols-2 w-[90vw]">
+    {/* Robotron Link */}
+    <div className="w-full">
+      <Link
+        to="third1"
+        spy={true}
+        smooth={true}
+        duration={200}
+        activeClass="active"
+        className="cursor-pointer"
       >
-        {/* Robotron Link */}
-        <div>
-          <Link
-            to="third1"
-            spy={true}
-            smooth={true}
-            duration={200}
-            activeClass="active"
-            className="cursor-pointer"
-          >
-            <button
-              onClick={() => setActiveSection("third1")}
-              className=" mt-5 relative inline-flex h-10 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
-              <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-xl font-medium text-white backdrop-blur-3xl gap-2 undefined  ">
-               Robotron
-              </span>
-            </button>
-          </Link>
-        </div>
+        <button
+          onClick={() => setActiveSection("third1")}
+          className="mt-5 relative inline-flex h-10 active:scale-95 transition overflow-hidden rounded-lg p-[1px] focus:outline-none"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
+          <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 lg:text-xl text-[20px] font-medium text-white backdrop-blur-3xl gap-2">
+            Robotron
+          </span>
+        </button>
+      </Link>
+    </div>
 
-        {/* Session Link */}
-        <div>
-          <Link
-            to="third2"
-            spy={true}
-            smooth={true}
-            duration={200}
-            activeClass="active"
-            className="cursor-pointer"
-          >
-            <button
-              onClick={() => setActiveSection("third2")}
-              className=" mt-5 relative inline-flex h-10 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
-              <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-xl font-medium text-white backdrop-blur-3xl gap-2 undefined  ">
-               Session
-              </span>
-            </button>
-          </Link>
-        </div>
+    {/* Session Link */}
+    <div className="w-[95vw] pl-5  ">
+      <Link
+        to="third2"
+        spy={true}
+        smooth={true}
+        duration={200}
+        activeClass="active"
+        className="cursor-pointer"
+      >
+        <button
+          onClick={() => setActiveSection("third2")}
+          className="mt-5 relative inline-flex h-10 active:scale-95 transition overflow-hidden rounded-lg p-[1px] focus:outline-none"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
+          <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 lg:text-xl text-[20px] font-medium text-white backdrop-blur-3xl gap-2">
+            Session
+          </span>
+        </button>
+      </Link>
+    </div>
 
-        {/* Projects Link */}
-        <div>
-          <Link
-            to="third3"
-            spy={true}
-            smooth={true}
-            duration={200}
-            activeClass="active"
-            className="cursor-pointer"
-          >
-            <button
-              onClick={() => setActiveSection("third3")}
-              className=" mt-5 relative inline-flex h-10 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
-              <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-xl font-medium text-white backdrop-blur-3xl gap-2 undefined  ">
-               Projects
-              </span>
-            </button>
-          </Link>
-        </div>
+    {/* Projects Link */}
+    <div className="w-full">
+      <Link
+        to="third3"
+        spy={true}
+        smooth={true}
+        duration={200}
+        activeClass="active"
+        className="cursor-pointer"
+      >
+        <button
+          onClick={() => setActiveSection("third3")}
+          className="mt-5 relative inline-flex h-10 active:scale-95 transition overflow-hidden rounded-lg p-[1px] focus:outline-none"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
+          <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 lg:text-xl text-[20px] font-medium text-white backdrop-blur-3xl gap-2">
+            Projects
+          </span>
+        </button>
+      </Link>
+    </div>
 
-        {/* Tecnoesis Link */}
-        <div>
-          <Link
-            to="third4"
-            spy={true}
-            smooth={true}
-            duration={200}
-            activeClass="active"
-            className="cursor-pointer"
-          >
-            <button
-              onClick={() => setActiveSection("third4")}
-              className=" mt-5 relative inline-flex h-10 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
-              <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-xl font-medium text-white backdrop-blur-3xl gap-2 undefined  ">
-               Tecnoesis
-              </span>
-            </button>
-          </Link>
-        </div>
-      </motion.div>
+    {/* Tecnoesis Link */}
+    <div className="w-full">
+      <Link
+        to="third4"
+        spy={true}
+        smooth={true}
+        duration={200}
+        activeClass="active"
+        className="cursor-pointer"
+      >
+        <button
+          onClick={() => setActiveSection("third4")}
+          className="mt-5 relative inline-flex h-10 active:scale-95 transition overflow-hidden rounded-lg p-[1px] focus:outline-none"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
+          <span className="font-spaced inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 lg:text-xl text-[20px] font-medium text-white backdrop-blur-3xl gap-2">
+            Tecnoesis
+          </span>
+        </button>
+      </Link>
+    </div>
+  </div>
+</motion.div>
+
     </>
   );
 };
--- src/Pages/Gallery/smallscreen.jsx
+++ src/Pages/Gallery/smallscreen.jsx
@@ -4,22 +4,22 @@ const SmallScreen = () => {
   return (
     <>
       <motion.div
-        initial={{ opacity: 0, translateX: "100%", translateY: "20%" }}
-        whileInView={{ opacity: 1, translateX: "-30%", translateY: "50%" }}
+        initial={{ opacity: 0, translateX: "-35%", translateY: "50%" }}
+        whileInView={{ opacity: 1, translateX: "-35%", translateY: "50%" }}
 
-        transition={{ duration: 1 }}
+        transition={{ duration: 4 }}
         className="relative h-[41vh] w-full bg-[url('https://res.cloudinary.com/dagggqd6g/image/upload/f_auto,q_auto/ntimquuholtlwaqknngo'),_url('src/Pages/Gallery/Rectangle_189.svg')] bg-[length:110%_90%] bg-center bg-no-repeat z-0 overflow-hidden"
       >
         
       </motion.div>
       
       <div className="relative z-50 block sm:hidden overflow-hidden">
-      <p className="font-ethenocentric font-normal heading-subsection gradient-text bg-gradient-to-r from-white via-pink-200 to-blue-400 bg-clip-text text-transparent  !text-[27px] mt-28 ml-4 px-3 ">
+      <p className="font-ethenocentric font-normal heading-subsection gradient-text bg-gradient-to-r from-white via-pink-200 to-blue-400 bg-clip-text text-transparent  !text-[27px] mt-28 ml-3 px-1 ">
   PHOTO GALLERY
 </p>
 
   
-  <p className="font-spaced font-bold heading-subsection gradient-text bg-gradient-to-r from-white via-pink-200 to-blue-400 bg-clip-text text-transparent text-[20px] ml-9 mt-1">
+  <p className="font-spaced font-bold heading-subsection gradient-text bg-gradient-to-r from-white via-pink-200 to-blue-400 bg-clip-text text-transparent text-[20px] ml-7 mt-1">
     A journey through timeless images
   </p>
 </div>
